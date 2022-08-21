215cbb2e172a27352c03f1d58855ea6517df07b7
Lena <110883931+ElenaGo1@users.noreply.github.com>
2022-08-21 17:09:09 +0300

    Квадрат числа

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Input integer number:");
+
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+int kvadrat = number * number;
+
+Console.WriteLine($"Квадрат числа {number} это {kvadrat}");
